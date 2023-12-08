817afa766dc4d387b06714301d644349dcb81afa
0HH02 <105432115+0HH02@users.noreply.github.com>
2023-12-08 18:03:11 -0500

    .

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,16 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+  name: 'strapi::cors',
+  config: {
+    enabled: true,
+    headers: '*', // o puedes especificar los headers permitidos
+    origin: ['http://localhost:3000', 'https://www.aimasstudio.com'], // reemplaza con tus dominios
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], // o puedes especificar los métodos permitidos
+    keepHeaderOnError: true,
+  }
+},
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
